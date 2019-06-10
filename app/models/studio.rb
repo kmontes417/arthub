feb98eb08b11a7fd8eb5f99119c1bd123f0bce70
pietro ventimiglia <twntymls@gmail.com>
2019-06-10 18:33:03 +0200

    added validations to models

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -2,4 +2,8 @@ class Studio < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :address, presence: true
+  validates :photo, presence: true
 end
