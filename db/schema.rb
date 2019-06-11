8586a3c57471f82f3402a3af6de85187ed67a0f2
luigi manganiello <luigi.manga92@gmail.com>
2019-06-11 11:37:34 +0200

    price added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_142142) do
+ActiveRecord::Schema.define(version: 2019_06_11_093525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_06_10_142142) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
