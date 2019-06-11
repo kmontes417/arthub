b30f1c4f468dbada7ae001140daae62280b3e714
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-11 17:20:55 +0200

    -worked on pages home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 # This is the dashboard
 class PagesController < ApplicationController
   def home
+    if current_user
+      @bookings = current_user.bookings.all
+      @studios = current_user.studios.all
+    end
   end
 end
