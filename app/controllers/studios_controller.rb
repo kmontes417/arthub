33f40a07aa10f9ccbcd84dd0890d08b0fc1a587f
alessio <albez93@gmail.com>
2019-06-11 17:37:30 +0200

    updated new and create method

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -5,7 +5,9 @@ class StudiosController < ApplicationController
   end
 
   def create
+    @user = current_user
     @studio = Studio.new(studio_params)
+    @studio.user = @user
     if @studio.save
       redirect_to dashbord_path(@dashbord)
     else
@@ -16,6 +18,6 @@ class StudiosController < ApplicationController
   private
 
   def studio_params
-    params.require(:studio).permit(:name)
+    params.require(:studio).permit(:name, :description, :address, :photo)
   end
 end
