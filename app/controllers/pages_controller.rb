32b0321e303b429c568294ee6a4074f16f0365d0
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-11 18:02:47 +0200

    fixed homepage and dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 # This is the dashboard
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
   def home
-    if current_user
-      @bookings = current_user.bookings.all
-      @studios = current_user.studios.all
-    end
+  end
+
+  def dashboard
+    @user = current_user
+      @bookings = @user.bookings
+      @studios = @user.studios
   end
 end
