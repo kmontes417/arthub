f5594d1a86c082d228efafde96a02d324be3e32a
pietro ventimiglia <twntymls@gmail.com>
2019-06-13 13:27:06 +0200

    working on the cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,17 @@ require 'date'
 #   Character.create(name: 'Luke', movie: movies.first)
 # User.destroy_all
 
+
+puts "Cleaning the database"
+
 Review.destroy_all
 Booking.destroy_all
 Studio.destroy_all
 User.destroy_all
 puts  "Cleaned DB"
+
+
+puts "Creating User seeds"
 
 5.times do
   User.create!(
@@ -24,6 +30,7 @@ puts  "Cleaned DB"
     host: true
     )
 end
+
 
   5.times do
   User.create!(
@@ -38,17 +45,26 @@ end
 
 ADRESSES = ["Piazza Duca D'Aosta, Milan", "Piazza Freud, 1, MIlan", "Piazza Bottini, 10, Milan", "Via G. B. Cassinis, 83, Milan", "Piazzale P. Genova,4, Milan", "Via Domodossola, 15, Milan", "Via Oroboni, 16, Milan", "Via Domodossola, 15, Milan", "Viale Molise 71, Milan", "Piazza Ascari, Milan" ]
 
-5.times do
-  Studio.create!(
-    name: Faker::Company.name,
-    category: ["recording studio", "photography studio", "dance studio"].sample ,
-    description: Faker::Name.last_name,
-    address: ADRESSES.sample,
-    photo: 'https://source.unsplash.com/random/?studio',
-    user: User.where(host: true).sample,
-    city: "milan",
-    )
-end
+# 5.times do
+#   Studio.create!(
+#     name: Faker::Company.name,
+#     category: ["recording studio", "photography studio", "dance studio"].sample ,
+#     description: Faker::Name.last_name,
+#     address: ADRESSES.sample,
+#     photo: 'https://source.unsplash.com/random/?studio',
+#     user: User.where(host: true).sample,
+#     city: "milan",
+#     )
+# end
+
+puts "Creating Studio seeds"
+
+Studio.create!(name: "Record Factory", category: "recording studio", description: "A good place to record your music in the middle of Milan city", address: "Corso Magenta, 52", remote_photo_url: "https://images.unsplash.com/photo-1503507865322-6c069305c4f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80", user: User.where(host: true).sample, price: 30, city: "milan")
+Studio.create!(name: "Graphy", category: "photography studio", description: "the perfect place to make awesome photos in the middle of Milan city", address: "via cufra, 8", remote_photo_url: "https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80", user: User.where(host: true).sample, price: 15, city: "milan")
+Studio.create!(name: "Felix Studio", category: "recording studio", description: "the perfect place to organize studio sessions with musicians", address: "Piazzale P. Genova,4", remote_photo_url: "https://images.unsplash.com/photo-1517273006195-51fa3364bce2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1566&q=80", user: User.where(host: true).sample, price: 32 , city: "milan")
+Studio.create!(name: "YourPhoto", category: "photography studio", description: "the perfect place to organize photography portraits", address: "Piazzale P. Genova,4", remote_photo_url: "https://images.unsplash.com/photo-1527011046414-4781f1f94f8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80", user: User.where(host: true).sample, price: 20, city: "milan")
+
+puts "Creating Booking seeds"
 
 10.times do
   Booking.create(
@@ -60,12 +76,17 @@ end
     )
 end
 
+puts "Creating Review seeds"
+
 10.times do
   Review.create(
     booking: Booking.all.sample,
     description: Faker::Quote.robin,
     rating: rand(1..5))
 end
+
+
+puts "db:seed completed"
 
 # Studio.new()
 # Booking.new()
