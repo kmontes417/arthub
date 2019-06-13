8d9701852636c4cb7a9398762e0203aecb32b7ab
krystal montesdeoca <krystal.montesdeoca@gmail.com>
2019-06-13 11:44:46 +0200

    working on adding stars to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,14 @@ class ReviewsController < ApplicationController
   def create
     @booking = Booking.find(params[:booking_id])
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.booking = @booking
     raise
     if @review.save
 
       redirect_to studio_path(@booking.studio)
     else
+      flash[:alert] = "Something went wrong."
       render :new
     end
   end
